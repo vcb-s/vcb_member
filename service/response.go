70e245ff3f91dc93da3804ceb2519471c0a2a6b9
inori-lover <337938188@qq.com>
2019-12-29 22:37:27 +0800

    不再强制赋值

--- service/response.go
+++ service/response.go
@@ -15,56 +15,63 @@ type JSONData struct {
 // NoDataResponse 没有数据
 func (j *JSONData) NoDataResponse(c *gin.Context) {
 	j.Code = http.StatusNoContent
-	j.Message = "没有数据"
+	if j.Message == "" {
+		j.Message = "没有数据"
+	}
 	c.JSON(http.StatusOK, j)
 }
 
 // BadRequest 参数错误
 func (j *JSONData) BadRequest(c *gin.Context) {
 	j.Code = http.StatusBadRequest
-	j.Message = "参数错误"
+	if j.Message == "" {
+		j.Message = "参数错误"
+	}
 	c.JSON(http.StatusOK, j)
 }
 
 // ResponseOK 请求成功
 func (j *JSONData) ResponseOK(c *gin.Context) {
 	j.Code = http.StatusOK
-	j.Message = "操作成功"
+	if j.Message == "" {
+		j.Message = "操作成功"
+	}
 	c.JSON(http.StatusOK, j)
 }
 
 // ServerError 服务器错误
 func (j *JSONData) ServerError(c *gin.Context) {
 	j.Code = http.StatusInternalServerError
-	j.Message = "服务出了一点小问题"
+	if j.Message == "" {
+		j.Message = "服务出了一点小问题"
+	}
 	c.JSON(http.StatusOK, j)
 }
 
 // Unauthorized 缺失认证
 func (j *JSONData) Unauthorized(c *gin.Context) {
 	j.Code = http.StatusUnauthorized
-	j.Message = ""
 	c.JSON(http.StatusOK, j)
 }
 
 // FailAuth 认证失败
 func (j *JSONData) FailAuth(c *gin.Context) {
 	j.Code = http.StatusForbidden
-	j.Message = ""
 	c.JSON(http.StatusOK, j)
 }
 
-// RepetitiveOperation 不能重复操作
-func (j *JSONData) RepetitiveOperation(c *gin.Context) {
+// NotAcceptable 无效操作
+func (j *JSONData) NotAcceptable(c *gin.Context) {
 	j.Code = http.StatusNotAcceptable
-	j.Message = "不能重复操作"
+	if j.Message == "" {
+		j.Message = "无效操作"
+	}
 	c.JSON(http.StatusOK, j)
 }
 
 // TimeOut 访问超时
 func (j *JSONData) TimeOut(c *gin.Context) {
 	j.Code = http.StatusRequestTimeout
-	j.Message = ""
 	c.JSON(http.StatusOK, j)
 }
 
