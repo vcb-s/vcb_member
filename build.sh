4ee325a6bcdfe3e76965df29dfd8d6739c788ce2
inori-lover <337938188@qq.com>
2020-01-04 19:25:10 +0800

    添加win构建命令示例

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o main main.go
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o build main.go
+# CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o build main.go
 
-# scp main inori@vcb-s.com:/www/vcbs_member/be/main.new
+# scp build inori@vcb-s.com:/www/vcbs_member/be/main.new
