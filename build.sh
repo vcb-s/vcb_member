50232f8603be94e0cb4868ea5ccfef6df68286fd
inori-lover <337938188@qq.com>
2020-01-05 15:32:53 +0800

    添加控制台输出

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
+echo "begin..."
+
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o build main.go
 # CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o build main.go
 
 # scp build inori@vcb-s.com:/www/vcbs_member/be/main.new
+
+echo "done"
