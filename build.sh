73ef1697c59817088265dd72bf5c2ca13aa41cee
inori-lover <337938188@qq.com>
2020-01-12 00:07:45 +0800

    修正上传命令

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ echo "begin..."
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o build main.go
 # CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o build main.go
 
-# scp build inori@vcb-s.com:/www/vcbs_member/be/main.new
+echo "upload..."
+scp build -P 30000 -2 inori@bupt.moe:/www/vcb-s/vcbs_member/be/
 
 echo "done"
