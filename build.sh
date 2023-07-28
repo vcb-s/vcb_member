9192bf7b0ca013ffa082e5a3e0f653b916e955d2
inori_lover <inori_lover@outlook.com>
2023-07-28 22:31:54 +0800

    doc: 补充scp速度问题说明

--- build.sh
+++ build.sh
@@ -11,6 +11,7 @@ CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o build main.go
 echo "build success"
 
 echo "uploading..."
+# windows bash 环境下的scp实在多少有点问题…会只剩下100k不到；linux/mac下边未遇见类似问题
 scp build vcb-s:/var/www/vcb-s/vcbs_member/be/build
 echo "upload success"
 
