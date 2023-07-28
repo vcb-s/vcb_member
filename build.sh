646110d57af57d82626aeb20a4b884ce4b53e3fd
bagolo6911 <bagolo6911@irebah.com>
2023-07-28 11:49:23 +0800

    feat: make deploy auto

--- build.sh
+++ build.sh
@@ -8,8 +8,16 @@ go env -w GOPROXY=https://goproxy.io,direct
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o build main.go
 # CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o build main.go
 
-# 不使用scp，上传不知道为何很慢很慢，sftp协议有3M/s，这里只有500kbps的平均值
-# echo "upload..."
-# scp build -P 30000 -2 inori@bupt.moe:/www/vcb-s/vcbs_member/be/
+echo "build success"
 
-echo "done"
+echo "uploading..."
+scp build vcb-s:/var/www/vcb-s/vcbs_member/be/build
+echo "upload success"
+
+echo "restarting..."
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+ssh -t vcb-s "cd /var/www/vcb-s/vcbs_member/be/ && . restart.sh"
+echo "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
+echo "restart success"
+
+echo "deploy success"
